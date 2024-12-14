98066c7fc09d1cf6caf704de46d5a0bdfff84f25
Timbaro9 <tmtmosweu@gmail.com>
2024-12-14 18:19:07 +0200

    Add image preview functionality to product form; implement Stimulus controller for dynamic image display

---
+++ app/javascript/controllers/image_preview_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="image-preview"
+export default class extends Controller {
+  static targets = ["input", "preview"];
+
+  connect() {
+    this.inputTarget.addEventListener("change", this.previewImages.bind(this));
+  }
+
+  previewImages() {
+    this.previewTarget.innerHTML = ''; // Clear the current images
+    const files = this.inputTarget.files;
+
+    Array.from(files).forEach(file => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        img.classList.add('radius-md');
+        this.previewTarget.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    });
+  }
+}
