9209a0020b0a9c75443a918c73773c436e845b53
Timbaro9 <tmtmosweu@gmail.com>
2024-12-18 21:20:00 +0200

    Update stock size validation to allow specific string values and numeric input

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,17 @@
 class Stock < ApplicationRecord
   belongs_to :product
 
-  validates :size, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  ALLOWED_SIZES = %w[XS S M L XL XXL XXXL].freeze
+
+  validates :size, presence: true
+  validate :size_must_be_string_or_number
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  private
+
+  def size_must_be_string_or_number
+    unless size.is_a?(Numeric) || (size.is_a?(String) && ALLOWED_SIZES.include?(size.upcase))
+      errors.add(:size, "must be a number or a string of the following values: #{ALLOWED_SIZES.join(', ')}")
+    end
+  end
 end
