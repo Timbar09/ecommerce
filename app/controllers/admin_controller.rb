8c16b95aadb61683d6c391e935947ff07c128c75
Timbaro9 <tmtmosweu@gmail.com>
2024-12-30 08:31:35 +0200

    Add admin profile show page and update routes for admin access

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,18 @@
 class AdminController < ApplicationController
   layout "admin"
   before_action :authenticate_admin!
+  before_action :set_admin, only: %i[ show ]
 
   def index
     @admins = Admin.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
   namespace :admin do
     resources :orders
     resources :products do
@@ -6,7 +12,7 @@ Rails.application.routes.draw do
     end
     resources :categories
   end
-  devise_for :admins
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -20,9 +26,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  authenticated :admin_user do
-    root to: "admin#index", as: :admin_root
-  end
-
   get "admin" => "admin#index"
+  get "admins/:id" => "admin#show", as: :admin_show
 end
