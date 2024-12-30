452147b96807cb4821a05a4e22f3c1c77d513f9d
Timbaro9 <tmtmosweu@gmail.com>
2024-12-30 08:24:40 +0200

    Refactor button and modal styles to use larger radius for improved UI consistency

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -3,16 +3,16 @@ module TableHelper
     show_action = actions.find { |action| action[:name].include?("View") }
 
     {
-      image: ->(item) { link_to image_tag(item.display_thumb_image, class: "table__image radius-md"), show_action[:path].call(item) },
+      image: ->(item) { link_to image_tag(item.display_thumb_image, class: "table__image radius-lg"), show_action[:path].call(item) },
       product_name: ->(item) { item.display_product_name },
       size: ->(item) { item.size.upcase },
       id: ->(item) { item.display_sku },
       price: ->(item) { number_to_currency(item.price) },
       total: ->(item) { number_to_currency(item.total) },
-      fulfilled: ->(item) { content_tag(:span, item.display_fulfilled, class: item.fulfilled ? "fulfilled radius-md" : "unfulfilled radius-md") },
+      fulfilled: ->(item) { content_tag(:span, item.display_fulfilled, class: item.fulfilled ? "fulfilled radius-lg" : "unfulfilled radius-lg") },
       actions: ->(item) {
         content_tag(:span, class: "table__actions--button", data: { controller: "modal" }) do
-          button_html = content_tag(:button, class: "modal__button", data: { action: "click->modal#toggle" }) do
+          button_html = content_tag(:button, class: "modal__button radius-lg", data: { action: "click->modal#toggle" }) do
             render "shared/icon", name: "ellipsis"
           end
 
@@ -21,9 +21,9 @@ module TableHelper
               actions.map do |action|
                 content_tag(:li, class: "modal__list--item") do
                   if action[:method] == :delete
-                    button_to action[:name], action[:path].call(item), method: :delete, class: "modal__list--link", data_action: "click->options-modal#close"
+                    button_to action[:name], action[:path].call(item), method: :delete, class: "modal__list--link radius-md", data_action: "click->options-modal#close"
                   else
-                    link_to action[:name], action[:path].call(item), method: action[:method], class: "modal__list--link", data_action: "click->options-modal#close"
+                    link_to action[:name], action[:path].call(item), method: action[:method], class: "modal__list--link radius-md", data_action: "click->options-modal#close"
                   end
                 end
               end.join.html_safe
