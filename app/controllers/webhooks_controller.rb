1294901f037b1e1e09106ad2f277e3c5b4607f2f
Timbaro9 <tmtmosweu@gmail.com>
2025-04-27 10:59:53 +0200

    Add WebhooksController to handle Stripe webhook events

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,57 @@
+class WebhooksController < ApplicationController
+  skip_forgery_protection only: :stripe
+
+  def stripe
+    payload = request.body.read
+    sig_header = request.headers["HTTP_STRIPE_SIGNATURE"]
+    endpoint_secret = Rails.application.credentials.dig(:stripe, :Webhook_secret)
+    event = nil
+
+    begin
+      event = Stripe::Webhook.construct_event(payload, sig_header, endpoint_secret)
+    rescue JSON::ParserError => e
+      render json: { error: "Invalid payload" }, status: 400 and return
+    rescue Stripe::SignatureVerificationError => e
+      render json: { error: "Invalid signature" }, status: 400 and return
+    end
+
+    case event["type"]
+    when "checkout.session.completed"
+      session = event["data"]["object"]
+      handle_checkout_session(session)
+    else
+      render json: { message: "Unhandled event type: #{event['type']}" }, status: 200 and return
+    end
+
+    render json: { message: "Success" }, status: 200
+  end
+
+  private
+
+  def handle_checkout_session(session)
+    shipping_details = session["shipping_details"]
+    address = "#{shipping_details['address']['line1']}, #{shipping_details['address']['city']}, #{shipping_details['address']['state']}, #{shipping_details['address']['postal_code']}"
+    order = Order.create(
+      customer_email: session["customer_email"],
+      total: session["amount_total"],
+      address: address,
+      fulfilled: false
+    )
+    full_session = Stripe::Checkout::Session.retrieve({
+      id: session["id"],
+      expand: [ "line_items" ]
+    })
+    line_items = full_session["line_items"]["data"]
+    line_items.each do |item|
+      product = Stripe::Product.retrieve(item["price"]["product"])
+      product_id = product["metadata"]["product_id"].to_i # Double-check this
+      OrderProduct.create(
+        order: order,
+        product_id: product_id,
+        quantity: item["quantity"],
+        size: product["metadata"]["size"]
+      )
+      ProductStock.find(product["metadata"]["product_id"]).decrement!(:amount, item["quantity"])
+    end
+  end
+end
