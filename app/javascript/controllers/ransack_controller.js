d93c67030cc7083eefc0f4e8ec29ce228c9e64a2
Timbaro9 <tmtmosweu@gmail.com>
2025-06-28 00:29:48 +0200

    Refactor Ransack input handling: update number type detection logic and streamline event listeners for improved functionality

--- app/javascript/controllers/ransack_controller.js
+++ app/javascript/controllers/ransack_controller.js
@@ -8,22 +8,24 @@ export default class extends Controller {
     // On every page reload, uncheck all checkboxes
     this.uncheckAll()
     this.searchButtonTarget.addEventListener("click", this.handleOpenSearch.bind(this))
-    // When the input ransack data-value-type is number, handle the input accordingly
-    if (this.inputTarget.dataset.ransackValueType === "number") {
-      this.inputTarget.addEventListener("input", (event) => {
-        const value = event.target.value.replace(/[^0-9.]/g, "")
-        event.target.value = value
-      })
-    }
-    this.inputTarget.addEventListener("blur", () => {
-      setTimeout(() => {
-        this.handleCloseSearch()
-      }, 250)
-    })
-    this.inputTarget.addEventListener("input", () => {
-      if (this.inputTarget.value.trim() === "") {
-        this.handleCloseSearch()
+    // When all the inputs ransack data-value-type is number, handle the input accordingly
+    this.inputTargets.forEach(input => {
+      if (input.dataset.ransackValueType === "number") {
+        input.addEventListener("input", (event) => {
+          const value = event.target.value.replace(/[^0-9.]/g, "")
+          event.target.value = value
+        })
       }
+      input.addEventListener("blur", () => {
+        setTimeout(() => {
+          this.handleCloseSearch()
+        }, 250)
+      })
+      input.addEventListener("input", () => {
+        if (input.value.trim() === "") {
+          this.handleCloseSearch()
+        }
+      })
     })
   }
 
