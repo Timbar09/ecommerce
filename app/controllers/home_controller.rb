384cfc8429c5cf49834a85d146515526c2eeec41
Timbaro9 <tmtmosweu@gmail.com>
2025-06-28 16:37:17 +0200

    Implement Ransack query in HomeController index action to enhance product filtering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+  include RansackSearchable
+
   def index
+    ransack_query(Product)
+    @products = @q.result(distinct: true)
     @categories = Category.all
   end
 end
