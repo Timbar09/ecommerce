036350fe87f7d7a722fee87f37f0149aa21d9dce
Timbaro9 <tmtmosweu@gmail.com>
2025-07-02 13:12:58 +0200

    Enhance user edit form: add fields for first name, last name, username, and phone number; update avatar upload section

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  ROLES = %w[user admin super_admin].freeze
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 50, 50 ]
+    attachable.variant :medium, resize_to_limit: [ 150, 150 ]
+  end
+
+  def display_thumb_image
+    avatar.attached? ? avatar.variant(:thumb) : "https://via.placeholder.com/50"
+  end
+
+  def display_medium_image
+    avatar.attached? ? avatar.variant(:medium) : "https://via.placeholder.com/150"
+  end
 end
