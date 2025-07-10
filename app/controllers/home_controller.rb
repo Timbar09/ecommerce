aeed654c6d3d000f58a5386b9dfa3954bd2a3c3c
Timbaro9 <tmtmosweu@gmail.com>
2025-07-10 19:10:21 +0200

    Refactor shop home layout: update header side structure and styles; add side product display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,7 @@ class HomeController < ApplicationController
 
     @hero_product = Product.find_by(id: 8) || Product.first
     @hero_product_category = @hero_product.category
+
+    @side_product = Product.find_by(id: 5) || Product.last
   end
 end
