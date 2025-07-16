cb5dbba8d048ea99aec6c9006722f638abb50783
Timbaro9 <tmtmosweu@gmail.com>
2025-07-16 18:54:53 +0200

    Update landing page height; Add '@popular_categories' local varible to home controller and update button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,12 @@ class HomeController < ApplicationController
   def index
     ransack_query(Product)
     @products = @q.result(distinct: true)
-    @categories = Category.all
 
     @hero_product = Product.find_by(id: 8) || Product.first
     @hero_product_category = @hero_product.category
 
     @side_product = Product.find_by(id: 5) || Product.last
+
+    @popular_categories = Category.limit(4)
   end
 end
