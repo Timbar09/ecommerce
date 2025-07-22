f9376f4bfa1c834819fd5f372c0cb7a2409c4d76
Timbaro9 <tmtmosweu@gmail.com>
2025-07-22 13:01:39 +0200

    Add products section to landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
 
     @side_product = Product.find_by(id: 5) || Product.last
 
-    @popular_categories = Category.limit(4)
+    @popular_categories = Category.limit(4).order("RANDOM()")
 
     @features = [
       { title: "Fast Delivery", description: "1-3 days.", icon: "truck" },
@@ -18,5 +18,8 @@ class HomeController < ApplicationController
       { title: "Pro Quality Support", description: "24/7 live support.", icon: "phone" },
       { title: "Secure payment", description: "SSL.", icon: "shield-check" }
     ]
+
+
+    @select_products = Product.limit(12).order("RANDOM()")
   end
 end
